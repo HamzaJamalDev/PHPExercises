95b5bf91f8300b0a964c4f7d81aa1d0c83d63ca7
Hamza Jamal <hamzajamal.phd@gmail.com>
2021-12-01 07:22:13 +0100

    Add comments to index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
 <body>
     <?php
+    // Déclarer les variables utilisées
     $image = 'logo.jpg';
     $titre1 = 'Accueil';
     $titre2 = 'Menu';
@@ -21,21 +22,27 @@
         "Contacts" => "contact.php"
     ];
     ?>
+    <!-- Afficher l'image, les titres et le menu -->
     <img src="<?php echo $image; ?>" alt="logo">
     <h1><?php echo $titre1; ?></h1>
     <h1><?php echo $titre2; ?></h1>
+    <!-- Le menu est affiché en utilisant une liste non ordonnée -->
     <ul>
         <?php
+        // Itérer sur le tableau $menu par la boucle foreach
         foreach ($menu as $text => $lien) :
         ?>
+            <!-- Afficher les éléments de la liste sous forme des liens -->
             <li>
                 <a href="<?= $lien; ?>"><?= $text; ?></a>
             </li>
         <?php
+        // Fin de la boucle (endforeach;)
         endforeach;
         ?>
     </ul>
     <!-- OU bien -->
+    <!-- À la place de séparer le PHP de l'HTML, on a utilisé un script PHP pour afficher tous les éléments nécessaires -->
     <ul>
         <?php
         foreach ($menu as $text => $lien) {
